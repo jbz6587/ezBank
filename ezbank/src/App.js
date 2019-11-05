b871aea217aa125ae981fa3886b6a471958b0684
Jarrod <jarrodcummings1@aol.com>
2019-11-05 16:09:14 -0500

    Filled out elements in log in page and began adding styling

--- ezbank/src/App.js
+++ ezbank/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import {
   Button,
   ButtonToobar,
@@ -11,6 +12,7 @@ import {
   Modal,
   Badge
 } from 'react-bootstrap';
+
 import {
   Browser as Router,
   Switch,
@@ -19,27 +21,14 @@ import {
   Link
 } from 'react-router-dom';
 
-import Login from './views/login'
+import Login from './views/login';
 import Home from './views/home';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }
--- ezbank/src/views/login.js
+++ ezbank/src/views/login.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import '../css/login.css';
 
 
 export default class Login extends Component {
+	
+	render() {
+		return(
+		<div>
+			<div id='LogInDiv'>
+				<h2 id='LogInTitle'>Log In</h2>
+				<form id='LogInForm'>
+					Email: <input className='LogInFormElement' type="email"/>
+					<br/>
+					Password: <input className='LogInFormElement' type="password"/>
+					<br/>
+					<button id='LogInButton'>Log In</button>
+					<br/>
+					<button id='RegisterButton'>Register</button>
+					<br/>
+					<a>Recover Username/Password</a>
+				</form>
+			</div>
+			<div><a>Contact Us</a></div>
+		</div>
+		
+		
+		)
+	}
 	
 }
