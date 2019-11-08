abdd9de28aeddade16bbe443bf46469e89868b3a
Joseph Zell <jbz6587@rit.edu>
2019-11-08 17:01:53 -0500

    Homepage and navbar on homepage, still a WIP

--- ezbank/src/App.js
+++ ezbank/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import {
   Button,
   ButtonToobar,
@@ -12,7 +11,6 @@ import {
   Modal,
   Badge
 } from 'react-bootstrap';
-
 import {
   Browser as Router,
   Switch,
--- ezbank/src/views/home.js
+++ ezbank/src/views/home.js
@@ -1,6 +1,62 @@
 import React, { Component } from 'react';
+import App from '.././App.js'
+import '../css/home.css';
+import {
+  Navbar,
+  Nav,
+} from 'react-bootstrap';
 
 
 export default class Home extends Component {
-	
+	constructor(props) {
+	        super(props)
+	      this.state = {
+	          user : "Joe Schmoe",
+	          accounts : {
+	          	account1 : { type : 'Checkings', balance : '3,000', accountid : '6513219447', accNickName : 'Pay the bills'},
+	          	account2 : { type : 'Savings', balance : '420,000', accountid : '6567218975', accNickName : 'Rainy Day Fund'}
+	          }
+	      };
+	}
+
+	render() {
+	return(
+		<div>
+			<Navbar bg="dark" variant="dark">
+	   		<Navbar.Brand href="/">eZ-Bank</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+	    	<Navbar.Collapse id="basic-navbar-nav">
+		    	<Nav className="mr-auto">
+		    		<Nav.Link href="/">Home</Nav.Link>
+		    		<Nav.Link href="/transfer">Transfer</Nav.Link>
+		    		<Nav.Link href="/deposit">Deposit</Nav.Link>
+		    		<Nav.Link href="/help">Help</Nav.Link>
+		    		<Nav.Link href="/profile">Profile/Settings</Nav.Link>
+		    	</Nav>
+	    	</Navbar.Collapse>
+	    	</Navbar>
+		<h1>Welcome {this.state.user}</h1>
+		<hr/>
+		<h2 id="accountsTitle">Accounts:</h2>
+		<div id="accountDiv" className="block">
+			<h3 id="accountTitle"> {this.state.accounts.account1.type}</h3>
+			<div>
+				<h4>Account : {this.state.accounts.account1.accNickName}</h4>
+				Avaliable Balance : ${this.state.accounts.account1.balance}
+				<br/>
+				<button type="button">View Details</button>
+			</div>
+		</div>
+		<div id="accountDiv" className="block">
+			<h3 id="accountTitle"> {this.state.accounts.account2.type} Account  Account Balance : ${this.state.accounts.account2.balance}</h3>
+			<div>
+				<h4>Account : {this.state.accounts.account2.accNickName}</h4>
+				Avaliable Balance : ${this.state.accounts.account2.balance}
+				<br/>
+				<button type="button">View Details</button>
+			</div>
+		</div>
+		</div>
+		)
+	}
 }
