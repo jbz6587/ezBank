17039b50f13d1b4ea131778f87af69f346e26cc8
Jarrod <jarrodcummings1@aol.com>
2019-11-08 22:18:25 -0500

    Added submit button and validation for pw recovery form. Added more styling. Page is complete, except for notifying user on successful submit

--- ezbank/src/views/pwrecovery.js
+++ ezbank/src/views/pwrecovery.js
@@ -8,12 +8,17 @@ export default class PWRecovery extends Component {
 		var fieldHtml = "";
 		
 		if(document.getElementById("EmailRadio").checked){
-			fieldHtml = "<form id='pwRecoveryForm'><label>Email:</label> <input type='email'/></form>";
+			fieldHtml = "<form id='pwRecoveryForm'> <label>Email:</label> <input required type='email'/> <button id='pwRecoveryBtn'>Submit</button> </form>";
 			document.getElementById("RecoveryField").innerHTML = fieldHtml;
 		}
 		else if (document.getElementById("PhoneNumRadio").checked){
-			fieldHtml = "<form id='pwRecoveryForm'><label>Phone Number:</label> <input type='tel'/></form>";
+			var regex = "^\\(?\\d{3}\\)?[\\s-]\\d{3}[\\s-]\\d{4}$";
+			fieldHtml = "<form id='pwRecoveryForm'> <label>Phone Number:</label> <input id='phoneNumInput' required/> <button id='pwRecoveryBtn'>Submit</button> </form>";
 			document.getElementById("RecoveryField").innerHTML = fieldHtml;
+			var field = document.getElementById("phoneNumInput");
+			field.pattern = regex;
+			field.title = "Valid formats include:\n XXX-YYY-ZZZZ \n or (XXX) YYY-ZZZZ \n or XXX YYY ZZZZ \n or (XXX)-YYY-ZZZZ";
+			
 		}
 		else {
 			document.getElementById("RecoveryField").innerHTML = "";
@@ -30,10 +35,18 @@ export default class PWRecovery extends Component {
 				<br/>
 				Instructions: To recover your forgotten password, please choose a recovery method and then enter your email address or phone number into the provided field.
 			</div>
-			Email: <input id="EmailRadio" type="radio" value="Email" name="RecoveryMethod" onClick={this.displayRecoveryField}/>
-			Phone Number: <input id="PhoneNumRadio" type="radio" value="Phone" name="RecoveryMethod" onClick={this.displayRecoveryField}/>
-			<div id="RecoveryField">
 			
+			<div id='inputsDiv'>
+				<label>Email:</label> <input id="EmailRadio" type="radio" value="Email" name="RecoveryMethod" onClick={this.displayRecoveryField}/>
+				<br/>
+				<label>Phone Number:</label> <input id="PhoneNumRadio" type="radio" value="Phone" name="RecoveryMethod" onClick={this.displayRecoveryField}/>
+				<br/>
+				<div id="RecoveryField">
+					
+				</div>
+			</div>
+			<div id="messageDiv">
+				Forgot your eMail and Phone number? Contact an eZ-Bank employee at 555-BANK or  support@eZ-Bank.com 
 			</div>
 		</div>
 		
