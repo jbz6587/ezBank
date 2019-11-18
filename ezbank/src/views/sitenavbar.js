98742d158ae770397a262440ecd8ffd441ea79e0
Joseph Zell <jbz6587@rit.edu>
2019-11-17 21:22:26 -0500

    testing netlify

--- ezbank/src/views/sitenavbar.js
+++ ezbank/src/views/sitenavbar.js
@@ -13,12 +13,12 @@ export default class SiteNavbar extends Component {
 				<Navbar bg="dark" variant="dark">
 					<Navbar.Brand className="navLogo" href="/home">eZ-Bank</Navbar.Brand>
 					<Nav className="mr-auto">
-						<Nav.Link className="navLink" href="/home">Home</Nav.Link>
-						<Nav.Link className="navLink" href="/transfer">Transfer</Nav.Link>
-						<Nav.Link className="navLink" href="/deposit">Deposit</Nav.Link>
-						<Nav.Link className="navLink" href="/help">Help</Nav.Link>
-						<Nav.Link className="navLink" href="/contact">Contact Us</Nav.Link>
-						<Nav.Link className="navLink" href="/profilesettings">Profile/Settings</Nav.Link>
+						<Nav.Link className="navLink" href="./home">Home</Nav.Link>
+						<Nav.Link className="navLink" href="./transfer">Transfer</Nav.Link>
+						<Nav.Link className="navLink" href="./deposit">Deposit</Nav.Link>
+						<Nav.Link className="navLink" href="./help">Help</Nav.Link>
+						<Nav.Link className="navLink" href="./contact">Contact Us</Nav.Link>
+						<Nav.Link className="navLink" href="./profilesettings">Profile/Settings</Nav.Link>
 						<a id="logoutButton" href="/">log out</a>
 					</Nav>
 
