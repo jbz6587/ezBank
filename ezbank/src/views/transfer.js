12a2205fb85183b05f4894b460605bf74230182d
Joseph Zell <jbz6587@rit.edu>
2019-11-17 23:31:14 -0500

    Added transfer form validation

--- ezbank/src/views/transfer.js
+++ ezbank/src/views/transfer.js
@@ -13,8 +13,15 @@ export default class Transfer extends Component {
 	        accounts : {
 				account1 : { type : 'Checkings', balance : '3,000', accountid : '6513219447', accNickName : 'Pay the bills'},
 	          	account2 : { type : 'Savings', balance : '420,000', accountid : '6567218975', accNickName : 'Rainy Day Fund'}
-	        }
+	        },
+	        fromAccount : "",
+	        toAccount : "",
+	        amount : 0
 	    };
+	    this.handleFromAccountChange = this.handleFromAccountChange.bind(this);
+	    this.handleToAccountChange = this.handleToAccountChange.bind(this);
+	    this.handleAmountChange = this.handleAmountChange.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	
 	toggleSchedulerDialog = () => {
@@ -24,6 +31,33 @@ export default class Transfer extends Component {
 		}
 		else {
 			dialog.style.display = "none";
+		}
+	}
+
+	handleFromAccountChange(event){
+		this.setState({fromAccount: event.target.value});
+	}
+
+	handleToAccountChange(event){
+		this.setState({toAccount: event.target.value});
+	}
+
+	handleAmountChange(event){
+		this.setState({amount: event.target.value});
+	}
+
+	handleSubmit(event){
+		event.preventDefault();
+		console.log(this.state);
+		var from = parseFloat(this.state.fromAccount);
+		var to = parseFloat(this.state.toAccount);
+		var amnt = parseFloat(this.state.amount);
+		if(amnt > from){
+			alert("Insufficient funds for this transaction.");
+		}else if(from == to){
+			alert("You cannot transfer to the same account.")
+		}else{
+			alert("Transfer Successful");
 		}
 	}
 	
@@ -42,22 +76,26 @@ export default class Transfer extends Component {
 						<button id="scheduleTransferBtn" onClick={this.toggleSchedulerDialog}>Schedule Transfers</button>
 					</div>
 					<br/>
-					<form id="transferForm">
+					<form id="transferForm" onSubmit={this.handleSubmit}>
 						<label>From:</label>
 						<br/>
-						<select id="transferFromAcctSel" className="transferFormElement">
+						<select id="transferFromAcctSel" className="transferFormElement" onChange={this.handleFromAccountChange}>
 							<option value="">-- Select Account --</option>
+							<option value='3000.00'>Pay the bills - $3,000</option>
+							<option value='420000.00'>Rainy Day Fund - $420,000</option>
 						</select>
 						<br/><br/>
 						<label>To:</label>
 						<br/>
-						<select id="transferToAcctSel" className="transferFormElement">
+						<select id="transferToAcctSel" className="transferFormElement" onChange={this.handleToAccountChange}>
 							<option value="">-- Select Account --</option>
+							<option value='3000.00'>Pay the bills - $3,000</option>
+							<option value='420000.00'>Rainy Day Fund - $420,000</option>
 						</select>
 						<br/><br/>
 						<label>Amount:</label>
 						<br/>
-						<input id="transferAmt" className="transferFormElement" type="number" step="0.01" min="0.00" required></input>
+						<input id="transferAmt" className="transferFormElement" type="number" step="0.01" min="0.00" onChange={this.handleAmountChange} required></input>
 						<br/><br/>
 						<button id="confirmTransferBtn">Confirm</button>
 						<Link to="./home"><button id="cancelTransferBtn">Cancel</button></Link>
