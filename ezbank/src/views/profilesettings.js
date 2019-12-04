93717dbc1d99f0407da9006e6a3130f4f4f26f14
Joseph Zell <jbz6587@rit.edu>
2019-12-03 20:12:09 -0500

    Form validation and password requirement for security changes

--- ezbank/src/views/profilesettings.js
+++ ezbank/src/views/profilesettings.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
 import '../css/profilesettings.css';
 import SiteNavbar from '../views/sitenavbar.js';
+import swal from 'sweetalert';
 
 export default class ProfileSettings extends Component {
 	
+	constructor(props) {
+	    super(props)
+	    this.state = {
+	    	secChanged : false
+	    };
+	    this.handleSecSettingsChange = this.handleSecSettingsChange.bind(this);
+	    this.handleSaveChanges = this.handleSaveChanges.bind(this);
+	}
+
+	handleSaveChanges(event){
+		event.preventDefault();
+		if(this.state.secChanged){
+			swal({
+				text : 'Security settings changed, please enter account password to save changes!',
+				  content: {
+    				element: "input",
+    				attributes: {
+      				placeholder: "Type your password",
+      				type: "password",
+    				},
+  				},
+			})
+			.then(pw => {
+				if(pw == "user"){
+					swal("Settings Saved!","","success");
+					this.setState({ secChanged : false });
+				}else{
+					swal("Save Failed","Incorrect Password","error");
+				}
+			});
+		}else{
+			swal("Settings Saved!","","success");
+		}
+	}
+
+	handleSecSettingsChange(event){
+		this.setState({ secChanged : true });
+	}
+
+	//Edit button that was removed
+	//<button id="profileEditButton">edit</button>
+
 	render() {
 		return(
 			<div>
@@ -28,11 +71,14 @@ export default class ProfileSettings extends Component {
 							<p>555-6789 </p>
 						</span>
 						<br/><br/>
-						<button id="profileEditButton">edit</button>
+						
 						<br/><br/>
 						<hr id="divideLine"/>
 					</div>
+
+					
 					<div id= "settingsContainer">
+					<form onSubmit={this.handleSaveChanges}>
 						<div id="notifContainer">
 							<h3>Notifications:</h3>
 							<span className= "SettingsLabels">
@@ -56,25 +102,26 @@ export default class ProfileSettings extends Component {
 							<h3>Security:</h3>
 							<div>
 								<span className="SecrurityItems">
-								<div>
-									<b>2-Factor Authentication</b><br/><br/>
-										<input type="checkbox" value="phoneNotifs1"/>Phone: ***-6789<br/>
-										<input type="checkbox" value="phoneNotifs2"/>Email: ***Shmoe@mail.com
-								</div>
-								<div>
-									<br/>
-									<b>Fingerprint Scanner</b><br/><br/>
-									<input type="checkbox" value="phoneNotifs1"/>Fingerprint1<br/><br/>
-								</div>
+									<div>
+										<b>2-Factor Authentication</b><br/><br/>
+											<input type="checkbox" onChange={this.handleSecSettingsChange} value="phoneNotifs1"/>Phone: ***-6789<br/>
+											<input type="checkbox" onChange={this.handleSecSettingsChange} value="phoneNotifs2"/>Email: ***Shmoe@mail.com
+									</div>
+									<div>
+										<br/>
+										<b>Fingerprint Scanner</b><br/><br/>
+										<input type="checkbox" onChange={this.handleSecSettingsChange} value="phoneNotifs1"/>Fingerprint1<br/><br/>
+									</div>
 								</span>
-								<button id="securityButton">Change Security Options</button><p id="instruction"> *requires password to be entered</p>
+								<p id="instruction"> *requires password to be entered</p>
 								<br/>
 							</div>
 						</div>
 						<div>
 							<br/>
-							<button id= "saveButton">Save Changes</button>
+							<button id="saveButton" type="submit">Save Changes</button>
 						</div>
+						</form>
 					</div>
 				</div>
 			</div>
